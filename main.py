3008534481feecd1cdf314ea7e9964a0a20c3cab
abc <chetanshere24@gmail.com>
2022-09-11 15:39:26 +0530

    this is my 1 st code push in git

---
+++ main.py
@@ -0,0 +1,12 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://chetanshere24:shere1234@cluster0.sqdy6as.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {"name": "Chetan",
+     "surname":"Shere",
+     "email": "chetanshere24@gmail.com"}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
---
+++ mongotest1.py
@@ -0,0 +1,4 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
